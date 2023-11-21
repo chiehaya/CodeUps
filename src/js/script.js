de6c2f58edb93a8182fe048788afcc97fc430c89
chiehaya <jakkiriko@yahoo.co.jp>
2023-11-21 15:41:13 +0900

    1120

--- src/js/script.js
+++ src/js/script.js
@@ -34,18 +34,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         $(".js-hamburger").removeClass("is-open");
     }
 // swiper
-    const mv_swiper = new Swiper(".js-mv-swiper", {
-        loop: true,
-        speed: 2000,
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-    });
     const campaign_swiper = new Swiper(".campaign__list .js-campaign-swiper", {
         slidesPerView: 'auto',
         spaceBetween: 16,
@@ -64,4 +52,113 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         }
         },
     });
+
+    var box = $('.js-colorbox'),
+    speed = 700;  
+ 
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+    box.each(function(){
+    $(this).append('<div class="is-color"></div>')
+    var color = $(this).find($('.is-color')),
+    image = $(this).find('img');
+    var counter = 0;
+    
+
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+        $(this).delay(200).animate({'width':'100%'},speed,function(){
+        image.css('opacity','1');
+        $(this).css({'left':'0' , 'right':'auto'});
+        $(this).animate({'width':'0%'},speed);
+        })
+        counter = 1;
+        }
+    });
+});
+const pageTop = $(".js-page-top");
+    pageTop.hide();
+    var scrollHeight;
+    var scrollPosition;
+    var footHeight;
+    $(window).scroll(function () {
+
+    if ($(this).scrollTop() > 100) {
+        pageTop.fadeIn();
+    } else {
+            pageTop.fadeOut();
+        }
+    scrollHeight = $(document).height();
+    scrollPosition = $(window).height() + $(window).scrollTop();
+    footHeight = $("footer").innerHeight();
+    if (scrollHeight - scrollPosition <= footHeight) {
+        // ページトップボタンがフッター手前に来たらpositionとfixedからabsoluteに変更
+            $(".page-top").css({
+            position: "absolute",
+            bottom: footHeight + 10,
+            });
+        } else {
+            $(".page-top").css({
+            position: "fixed",
+            bottom: "35px",
+            });
+        }
+    });
+
+    // 定義した変数がページをロードしてからどのくらいの時間で実行するかを設定する
+        function loadRight() {
+            $('.loader__img-right').addClass("is-fade-up");
+        }
+        function loadLeft() {
+            $('.loader__img-left').addClass("is-fade-up");
+        }
+        function end_title() {
+            $('.loader__title-wrap').fadeOut(800);
+        }
+        function end_load() {
+            $('.loader').fadeOut(800);
+        }
+        function start_header() {
+            $(".header").addClass("is-show");
+        }
+        function swiper() {
+            const mv_swiper = new Swiper(".js-mv-swiper", {
+                loop: true,
+                speed: 2000,
+                effect: "fade",
+                fadeEffect: {
+                    crossFade: true,
+                },
+                autoplay: {
+                    delay: 4000,
+                    disableOnInteraction: false,
+                },
+            });
+        }
+            $(window).on('load', function () {
+            setTimeout(function () {
+                loadLeft();
+            }, 1000)
+
+            setTimeout(function () {
+                loadRight();
+            }, 1200)
+            setTimeout(function () {
+                end_title();
+            }, 1200)
+            setTimeout(function () {
+                end_load();
+            }, 2000)
+            setTimeout(function () {
+                start_header();
+            }, 5000)
+            setTimeout(function () {
+                swiper();
+            }, 4000);
+            
+        })
+        
+    
 });
